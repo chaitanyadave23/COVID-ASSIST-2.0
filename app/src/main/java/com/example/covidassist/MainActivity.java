b17d49223cc075bff45e281b6e228dc1854fcc8b
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-04-26 16:51:15 +0530

    Firebase PhoneAuth UI

--- app/src/main/java/com/example/covidassist/MainActivity.java
+++ app/src/main/java/com/example/covidassist/MainActivity.java
@@ -1,15 +1,92 @@
 package com.example.covidassist;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonPhoneAuth;
+    private static final int RC_SIGN_IN = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPhoneAuth = findViewById(R.id.buttonPhoneAuth);
+        // Set button listen
+        buttonPhoneAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doPhoneLogin();
+            }
+        });
+    }
 
+    private void doPhoneLogin() {
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                .setAvailableProviders(Collections.singletonList(
+                        new AuthUI.IdpConfig.PhoneBuilder().build()))
+                .setLogo(R.mipmap.ic_launcher)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                showAlertDialog(user);
+            } else {
+                /**
+                 *   Sign in failed. If response is null the user canceled the
+                 *   sign-in flow using the back button. Otherwise check
+                 *   response.getError().getErrorCode() and handle the error.
+                 */
+                Toast.makeText(getBaseContext(), "Phone Auth Failed", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void showAlertDialog(FirebaseUser user) {
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(
+                MainActivity.this);
+
+        // Set Title
+        mAlertDialog.setTitle("Successfully Signed In");
+
+        // Set Message
+        mAlertDialog.setMessage(" Phone Number is " + user.getPhoneNumber());
+
+        mAlertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        mAlertDialog.create();
+
+        // Showing Alert Message
+        mAlertDialog.show();
     }
 }

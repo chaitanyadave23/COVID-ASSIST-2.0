5b857bb372345670c19dcd45de79bd269d32d119
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-05-03 23:38:23 +0530

    Floating Action Button

--- app/src/main/java/com/example/covidassist/UserFeedActivity.java
+++ app/src/main/java/com/example/covidassist/UserFeedActivity.java
@@ -2,6 +2,7 @@ package com.example.covidassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,36 +18,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 
 public class UserFeedActivity extends AppCompatActivity {
-    private TextView title, name, age;
-    private Button btn;
-
+    FloatingActionButton add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feed);
-
-        title = (TextView) findViewById(R.id.Title);
-        age = (TextView) findViewById(R.id.age);
-        name = (TextView) findViewById(R.id.name);
-
-        btn = (Button) findViewById(R.id.button);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        add = findViewById(R.id.floatingActionAdd);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HashMap<String, Object> map = new HashMap<>();
-                map.put("title", title.getText().toString());
-                map.put("name", name.getText().toString());
-                map.put("title", age.getText().toString());
-
-                FirebaseDatabase.getInstance().getReference().child("Student").setValue(map);
-
-
-
+                Intent intent = new Intent(getApplicationContext(), AddNewFeedActivity.class);
+                startActivity(intent);
             }
         });
-
-
     }
 
 }

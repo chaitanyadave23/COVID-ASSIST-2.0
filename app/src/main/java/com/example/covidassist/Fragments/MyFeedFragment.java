7ba912781d03dc865c99caab1cce130dbae671f3
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-05-31 13:27:17 +0530

    -

--- app/src/main/java/com/example/covidassist/Fragments/MyFeedFragment.java
+++ app/src/main/java/com/example/covidassist/Fragments/MyFeedFragment.java
@@ -2,13 +2,29 @@ package com.example.covidassist.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.covidassist.Adapters.MyFeedAdapter;
+import com.example.covidassist.Model.feed;
 import com.example.covidassist.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,9 +43,43 @@ public class MyFeedFragment extends Fragment {
     MyFeedAdapter adapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = (RecyclerView) getView().findViewById(R.id.myRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        list = new ArrayList<feed>();
+        reference = FirebaseDatabase.getInstance().getReference().child("UserFeed");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    feed f = dataSnapshot1.getValue(feed.class);
+                    final String fuid = fuser.getUid();
+                    if(f.getUser_id().equals(fuid)) list.add(f);
+                    //hello
+
+                }
+
+                adapter = new MyFeedAdapter(getActivity(),list);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Toast.makeText(getActivity(), "Something is wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

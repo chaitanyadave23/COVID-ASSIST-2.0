ae84ed74ecec05daface03ee4a27f2c120759b83
NileshSikri <nilesh.kumarsikri@gmail.com>
2020-06-30 00:43:17 +0530

    Added location to userfeed

--- app/src/main/java/com/example/covidassist/Model/feed.java
+++ app/src/main/java/com/example/covidassist/Model/feed.java
@@ -5,15 +5,35 @@ public class feed {
     private String item_quantity;
     private String item_desc;
     private String user_id;
+    private double longitude;
+    private double latitude;
 
     public feed() {
     }
 
-    public feed(String item_name, String item_quantity, String item_desc, String user_id) {
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public feed(String item_name, String item_quantity, String item_desc, String user_id, double longitude, double latitude) {
         this.item_name = item_name;
         this.item_quantity = item_quantity;
         this.item_desc = item_desc;
         this.user_id = user_id;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public String getUser_id() {

e29cc7ee160a18e732876c9e3aa0ef966c134a8f
iamchaitanyadave <chaitanyadave23@gmail.com>
2020-11-01 15:28:59 +0530

    Collapsing toolbar on scroll

--- app/src/main/java/com/example/covidassist/MainActivity.java
+++ app/src/main/java/com/example/covidassist/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("CovidAssist");
         setSupportActionBar(toolbar);
 
         tabLayout = findViewById(R.id.tabLayout);
